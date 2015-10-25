f0f48fae4ac2c07d6f75ac99ff91e5efd6009394
Nandi <Nandi@Nandi-PC>
2015-10-25 13:38:30 +0100

    Logging

--- src/main/app/AppStart.java
+++ src/main/app/AppStart.java
@@ -5,5 +5,6 @@ import bean.ExpenseType;
 public class AppStart {
 	public static void main(String[] args) {
 		System.out.println(new ExpenseType("a", BigDecimal.TEN));
+		System.out.println("Success");
 	}
 }
