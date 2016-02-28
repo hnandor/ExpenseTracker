96dfe00d40d3352c744d1ae9d33b30ea09cd54a2
Nandi <huszka.nandor@gmail.com>
2016-02-28 14:28:18 +0100

    GUI buttons to list Expenses and ExpenseTypes

--- src/main/java/com/nhuszka/expense_tracker/gui/ExpenseTrackerGUI.java
+++ src/main/java/com/nhuszka/expense_tracker/gui/ExpenseTrackerGUI.java
@@ -1,6 +1,8 @@
 package com.nhuszka.expense_tracker.gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import org.springframework.stereotype.Component;
@@ -15,6 +17,20 @@ public class ExpenseTrackerGUI {
 
 	public ExpenseTrackerGUI() {
 		frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton button = new JButton("Types");
+		button.addActionListener(e -> {
+			System.out.println(serviceFacade.listExpenseType());
+		});
+		panel.add(button);
+
+		JButton button2 = new JButton("Expenses");
+		button2.addActionListener(e -> {
+			System.out.println(serviceFacade.listExpense());
+		});
+		panel.add(button2);
+		frame.add(panel);
+		frame.pack();
 	}
 
 	public void start(ServiceFacade serviceFacade) {
